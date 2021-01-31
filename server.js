8a38f47e29f20e75dbe83c54ee6bf8ffafbe4eb6
h1ghn0on <you@example.com>
2021-01-31 21:28:34 +0300

    супер сильно устал

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const middlewares = jsonServer.defaults({
+    static: './build',
+});
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
